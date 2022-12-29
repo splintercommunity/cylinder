e0415f21d9e1d88ffefd952cddada3c1e0661a3e
Shawn T. Amundson <amundson@bitwise.io>
2022-12-29 12:44:22 -0600

    Move handle_jwt_generate to commands::jwt_generate module
    This cleans up the CLI implementation, leaving room for additional
    commands to be added later.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -16,20 +16,14 @@
  * ------------------------------------------------------------------------------
  */
 
+mod commands;
 mod error;
 
-use std::path::Path;
-
 use clap::{Parser, Subcommand};
-use cylinder::jwt::JsonWebTokenBuilder;
-use cylinder::secp256k1::Secp256k1Context;
-use cylinder::{
-    current_user_key_name, current_user_search_path, load_key, load_key_from_path, Context,
-    PrivateKey,
-};
 use flexi_logger::{LogSpecBuilder, Logger};
 use log::LevelFilter;
 
+use commands::handle_jwt_generate;
 use error::CliError;
 
 /// Cylinder CLI
@@ -101,50 +95,6 @@ fn main() -> Result<(), CliError> {
             JwtCommands::Generate { key } => handle_jwt_generate(key),
         },
     }
-}
-
-fn handle_jwt_generate(key_name: Option<String>) -> Result<(), CliError> {
-    let private_key = load_private_key(key_name)?;
-
-    let context = Secp256k1Context::new();
-    let signer = context.new_signer(private_key);
-
-    let encoded_token = JsonWebTokenBuilder::new().build(&*signer).map_err(|err| {
-        CliError::from_source_with_message(Box::new(err), "failed to build json web token".into())
-    })?;
-
-    println!("{}", encoded_token);
-
-    Ok(())
-}
-
-fn load_private_key(key_name_opt: Option<String>) -> Result<PrivateKey, CliError> {
-    if let Some(key_name) = key_name_opt.as_ref() {
-        if key_name.contains('/') {
-            return load_key_from_path(Path::new(&key_name))
-                .map_err(|err| CliError::from_source(Box::new(err)));
-        }
-    }
-
-    load_key(
-        &key_name_opt
-            .map(String::from)
-            .unwrap_or_else(current_user_key_name),
-        &current_user_search_path(),
-    )
-    .map_err(|err| CliError::from_source(Box::new(err)))?
-    .ok_or_else(|| {
-        CliError::with_message({
-            format!(
-                "No signing key found in {}. Specify a valid key with the --key argument",
-                current_user_search_path()
-                    .iter()
-                    .map(|path| path.as_path().display().to_string())
-                    .collect::<Vec<String>>()
-                    .join(":")
-            )
-        })
-    })
 }
 
 pub fn log_format(
