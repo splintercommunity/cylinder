9acfa7201ec8223b4866fa2b430eaf3be5a2d905
Shawn T. Amundson <amundson@bitwise.io>
2022-12-29 12:20:34 -0600

    Update to clap 4 using derive macros
    Implementation is similar in purpose but now with derive macros which
    are much cleaner. A slight differences in implementation exists with
    respect to the verbose/quiet options meaning add/subtract verbosity,
    instead of quiet overriding verbose. Minor help wording changes are also
    made.
    
    Signed-off-by: Shawn T. Amundson <amundson@bitwise.io>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 /*
  * Copyright 2021 Cargill Incorporated
+ * Copyright 2022 Bitwise IO, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,16 +16,11 @@
  * ------------------------------------------------------------------------------
  */
 
-#[macro_use]
-extern crate clap;
-#[macro_use]
-extern crate log;
-
 mod error;
 
-use std::env;
 use std::path::Path;
 
+use clap::{Parser, Subcommand};
 use cylinder::jwt::JsonWebTokenBuilder;
 use cylinder::secp256k1::Secp256k1Context;
 use cylinder::{
@@ -32,43 +28,56 @@ use cylinder::{
     PrivateKey,
 };
 use flexi_logger::{LogSpecBuilder, Logger};
+use log::{error, info, LevelFilter};
 
 use error::CliError;
 
-const APP_NAME: &str = env!("CARGO_PKG_NAME");
-const VERSION: &str = env!("CARGO_PKG_VERSION");
+/// Cylinder CLI
+#[derive(Parser)]
+#[command(version)]
+#[command(propagate_version = true)]
+pub struct CylArgs {
+    /// Increase verbosity level
+    #[arg(long, short = 'v', action = clap::ArgAction::Count, global = true)]
+    verbose: u8,
+
+    /// Decrease verbosity level
+    #[arg(long, short = 'q', alias = "silent",  action = clap::ArgAction::Count, global = true)]
+    quiet: u8,
+
+    #[command(subcommand)]
+    commands: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate/examine Cylinder JWT (JSON web tokens)
+    Jwt {
+        #[command(subcommand)]
+        commands: JwtCommands,
+    },
+}
+
+#[derive(Subcommand)]
+enum JwtCommands {
+    /// Generates a JWT for a given private key
+    Generate {
+        /// Name or path of private key
+        #[arg(long, short = 'k')]
+        key: Option<String>,
+    },
+}
 
 fn main() {
-    let matches = clap_app!(myapp =>
-        (name: APP_NAME)
-        (version: VERSION)
-        (author: "Cargill")
-        (about: "Cylinder CLI")
-        (@arg verbose: -v +multiple +global "Log verbosely")
-        (@arg quiet: -q --quiet +global "Do not display output")
-        (@setting SubcommandRequiredElseHelp)
+    let args = CylArgs::parse();
 
-        (@subcommand jwt =>
-            (about: "Subcommands for generating and examining Cylinder JSON web tokens")
-            (@setting SubcommandRequiredElseHelp)
-            (@subcommand generate =>
-            (about: "Generates a JWT for a given private key")
-             (@arg private_key_file: -k --key +takes_value
-                        "Name or path of private key")
-            )
-        )
-
-    )
-    .get_matches();
-
-    // set default to info
-    let log_level = if matches.is_present("quiet") {
-        log::LevelFilter::Error
-    } else {
-        match matches.occurrences_of("verbose") {
-            0 => log::LevelFilter::Info,
-            1 => log::LevelFilter::Debug,
-            _ => log::LevelFilter::Trace,
+    let log_level = {
+        match args.verbose as i8 - args.quiet as i8 {
+            i8::MIN..=-2 => LevelFilter::Error,
+            -1 => LevelFilter::Warn,
+            0 => LevelFilter::Info,
+            1 => LevelFilter::Debug,
+            2..=i8::MAX => LevelFilter::Trace,
         }
     };
 
@@ -87,10 +96,10 @@ fn main() {
         Err(err) => panic!("Failed to start logger: {}", err),
     }
 
-    let res = match matches.subcommand() {
-        ("jwt", Some(matches)) => handle_jwt_subcommands(matches),
-        // Clap will have caught any unrecognized subcommands
-        (_, _) => unreachable!(),
+    let res = match args.commands {
+        Commands::Jwt { commands } => match commands {
+            JwtCommands::Generate { key } => handle_jwt_generate(key),
+        },
     };
 
     if let Err(err) = res {
@@ -98,17 +107,7 @@ fn main() {
     }
 }
 
-fn handle_jwt_subcommands(matches: &clap::ArgMatches) -> Result<(), CliError> {
-    match matches.subcommand() {
-        ("generate", Some(matches)) => handle_jwt_generate(matches),
-        // Clap will have caught any unrecognized subcommands
-        (_, _) => unreachable!(),
-    }
-}
-
-fn handle_jwt_generate(matches: &clap::ArgMatches) -> Result<(), CliError> {
-    let key_name = matches.value_of("private_key_file");
-
+fn handle_jwt_generate(key_name: Option<String>) -> Result<(), CliError> {
     let private_key = load_private_key(key_name)?;
 
     let context = Secp256k1Context::new();
@@ -123,16 +122,16 @@ fn handle_jwt_generate(matches: &clap::ArgMatches) -> Result<(), CliError> {
     Ok(())
 }
 
-fn load_private_key(key_name: Option<&str>) -> Result<PrivateKey, CliError> {
-    if let Some(key_name) = key_name {
+fn load_private_key(key_name_opt: Option<String>) -> Result<PrivateKey, CliError> {
+    if let Some(key_name) = key_name_opt.as_ref() {
         if key_name.contains('/') {
-            return load_key_from_path(Path::new(key_name))
+            return load_key_from_path(Path::new(&key_name))
                 .map_err(|err| CliError::from_source(Box::new(err)));
         }
     }
 
     load_key(
-        &key_name
+        &key_name_opt
             .map(String::from)
             .unwrap_or_else(current_user_key_name),
         &current_user_search_path(),
